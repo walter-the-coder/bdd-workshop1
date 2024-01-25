0272df00581f0f582837385558b583a1254eae05
Walter Kjell Dahl <walter.dahl@skatteetaten.no>
2024-01-25 15:07:53 +0100

    Added a simple validator

--- src/main/java/com/bdd/workshop/controller/dto/ReceptionResponse.java
+++ src/main/java/com/bdd/workshop/controller/dto/ReceptionResponse.java
@@ -1,11 +1,31 @@
 package com.bdd.workshop.controller.dto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.bdd.workshop.type.OrganisationNumber;
 
 public record ReceptionResponse(
-    String message
+    String message,
+    Map<String, String> validationErrors
 ) {
+    public ReceptionResponse {
+        if (validationErrors == null) {
+            validationErrors = new HashMap<>();
+        }
+    }
+
     public ReceptionResponse(OrganisationNumber organisationNumber) {
-        this("The data for organisation number " + organisationNumber.toString() + " was submitted successfully. ");
+        this(
+            "The data for organisation number " + organisationNumber.toString() + " was submitted successfully.",
+            new HashMap<>()
+        );
+    }
+
+    public ReceptionResponse(Map<String, String> validationErrors) {
+        this(
+            "The data did not validate.",
+            validationErrors
+        );
     }
 }
